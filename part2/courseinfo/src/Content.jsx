a023dc83e64c729cf9dff909cdb537e00c312b72
John <jsmillie23@gmail.com>
2025-05-22 14:54:34 -0500

    exercise 2.2 complete

--- part2/courseinfo/src/Content.jsx
+++ part2/courseinfo/src/Content.jsx
@@ -2,9 +2,17 @@ import { Part } from "./Part"
 
 
 const Content = ({parts}) => {
+  let total = 0;
   return (
     <div>
-      {parts.map(el => <Part key={el.id} part={el} />) }
+      {
+        parts.map(el => {
+          total += el.exercises;
+          return (
+           <Part key={el.id} part={el} />
+          )}
+      )}
+      <p><b>Total of {total} exercises</b></p>
     </div>
   )
     
